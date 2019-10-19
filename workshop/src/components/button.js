4d527ea1482be8146c17832b7e281cb3c275e14b
Alex <alexcarbajo@gmail.com>
2019-10-19 16:40:24 +0200

    base

--- workshop/src/components/button.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default props => 
-<a 
-    className={`c-Button c-Button--${props.type}`}
-    href={`props.href`}
-    rel="noopener noreferrer">{props.title}
-</a>
--- workshop/src/pages/index.js
+++ workshop/src/pages/index.js
@@ -1,16 +1,75 @@
 import React from "react"
 import { Link } from "gatsby"
-import ItemList from "../components/itemList"
-import Header from "../components/header"
-import Button from "../components/button"
 
 export default () => 
-    <div class="o-Layout">
-        <div class="o-Layout__item">
-            <Header headerText="Workshop" />
-            <ItemList />
+    <div className="o-dashboard">
+        <div className="o-dashboard__sidebar">
+            Sidebar
+        </div>
 
-            <Button type="primary" title="Button" href="#">asd</Button>
-            <Link className="c-Link c-Link--highlight" to="/contact/">Contact</Link>
+        <div className="o-dashboard__content">
+            <div className="o-dashboard__header">
+                <input type="text" className="c-search" placeholder="Find component" />
+                <select className="c-select">
+                    <option>EN</option>
+                    <option>AR</option>
+                </select>
+
+                <select className="c-select">
+                    <option>CLX</option>
+                    <option>SAIB</option>
+                    <option>BAJ</option>
+                    <option>NCB</option>
+                </select>
+                <select className="c-select">
+                <option>Default</option>
+                </select>
+            </div>
+
+            <div className="o-dashboard__item">
+                El iframe
+                
+                <div className="o-group">
+                    <div className="o-group__item">
+                        <div className="c-title">
+                            Shared
+                        </div>
+                        <div className="c-component">
+                            <div className="c-component__title">
+                                Layout
+                            </div>
+
+                            <ul className="c-component__list">
+                                <li className="c-component__list-item">
+                                    Grid
+                                </li>
+                                <li className="c-component__list-item">
+                                    Shell
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                    
+                <div className="o-group__item">
+                    <div className="c-title">
+                    Bocadillo
+                    </div>
+                    <div className="c-component">
+                        <div className="c-component__title">
+                            Churrasco
+                        </div>
+
+                        <ul className="c-component__list">
+                            <li className="c-component__list-item">
+                                Grid
+                            </li>
+                            <li className="c-component__list-item">
+                                Shell
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
